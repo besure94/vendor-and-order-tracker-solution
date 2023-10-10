23c3a4a07c68dd6b208f513616a2406ef0b97317
Brian Scherner <brianscherner@gmail.com>
2023-10-10 15:22:53 -0700

    Add functionality to create a new Vendor.

--- VendorAndOrderTracker/Controllers/VendorsController.cs
+++ VendorAndOrderTracker/Controllers/VendorsController.cs
@@ -19,5 +19,12 @@ namespace VendorAndOrderTracker.Controllers
     {
       return View();
     }
+
+    [HttpPost("/vendors")]
+    public ActionResult Create(string vendorName, string vendorDescription)
+    {
+      Vendor newVendor = new Vendor(vendorName, vendorDescription);
+      return RedirectToAction("Index");
+    }
   }
 }
